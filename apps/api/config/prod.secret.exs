cf31fec17d77ca24a8d165926ce9bbdb01dec976
Thiago Araujo <worf.mogh@gmail.com>
2018-03-20 10:02:41 -0700

    secrets

---
+++ apps/api/config/prod.secret.exs
@@ -0,0 +1,16 @@
+use Mix.Config
+
+# In this file, we keep production configuration that
+# you'll likely want to automate and keep away from
+# your version control system.
+#
+# You should document the content of this
+# file or create a script for recreating it, since it's
+# kept out of version control and might be hard to recover
+# or recreate for your teammates (or yourself later on).
+config :api, ApiWeb.Endpoint,
+  secret_key_base: "7d1+wKE/nuLfqMUfYdehEiQhInZDln9NPglZv7nYR0c2HcsFplgPJstqJiOCdQAX",
+  hostname: "${DB_HOST}",
+  username: "${DB_USER}",
+  password: "${DB_PASS}",
+  database: "${DB_NAME}"
